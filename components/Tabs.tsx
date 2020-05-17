97d427d686593ac7acbcdad0a73402bbae04d75c
Rahavee <rmanian@uvm.edu>
2020-05-16 20:48:21 -0400

    the tab highlight is rounded :)

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -1,86 +1,103 @@
 import React from "react";
 import styles from "./tabs.module.scss";
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import DashboardIcon from "@material-ui/icons/DashboardRounded";
 import SearchIcon from "@material-ui/icons/SearchRounded";
 import PortfolioIcon from "@material-ui/icons/AccountCircleRounded";
 
 import useTheme from "@material-ui/core/styles/useTheme";
+
 interface IProps {
-  currentPage: "dashboard" | "search" | "portfolio";
+    currentPage: "dashboard" | "search" | "portfolio";
 }
 
 const defaultStyles = {
-  fontSize: 20,
-  height: 70,
-  width: "100%",
-  marginTop: 20,
-  marginBottom: 20,
-  padding: 20,
-  borderRight: "red 5px solid",
-  cursor: "pointer"
+    fontSize: 20,
+    height: 70,
+    width: "95%",
+    marginTop: 20,
+    marginBottom: 20,
+    padding: 20,
+    cursor: "pointer"
 };
 
-export default function Tabs({ currentPage }: IProps) {
-  const router = useRouter();
-  const theme = useTheme();
-  return (
-    <div className={styles.root}>
-      <img
-        src={
-          "https://lh3.googleusercontent.com/proxy/mdrER7gpHRg7DAj8ySS-yvlnJslI-EF5Vhoz6WgxJfoMqvTO_pJf_BAvcxp5TgSvIUH6SOXp_Tze4zllwC0Oui9NLEndIAn5Ty67cndDfs1G-dme7zpeNcTITmqIsw"
-        }
-        alt={"Logo"}
-      />
+const currentTab = {
+    marginTop:15,
+    marginBottom:15,
+    width: 5,
+    borderRadius:5,
+    height: 80,
+};
 
-      <div className={styles.buttons}>
-        <DashboardIcon
-          onClick={() => {
-            router.push("/dashboard");
-          }}
-          style={{
-            ...defaultStyles,
-            color:
-              currentPage === "dashboard"
-                ? theme.palette.primary.main
-                : "#5B5E86",
-            borderRight:
-              currentPage === "dashboard"
-                ? `${theme.palette.primary.main} 5px solid`
-                : "none"
-          }}
-        />
-        <SearchIcon
-          onClick={() => {
-            router.push("/search");
-          }}
-          style={{
-            ...defaultStyles,
-            color:
-              currentPage === "search" ? theme.palette.primary.main : "#5B5E86",
-            borderRight:
-              currentPage === "search"
-                ? `${theme.palette.primary.main} 5px solid`
-                : "none"
-          }}
-        />
-        <PortfolioIcon
-          onClick={() => {
-            router.push("/portfolio");
-          }}
-          style={{
-            ...defaultStyles,
-            color:
-              currentPage === "portfolio"
-                ? theme.palette.primary.main
-                : "#5B5E86",
-            borderRight:
-              currentPage === "portfolio"
-                ? `${theme.palette.primary.main} 5px solid`
-                : "none"
-          }}
-        />
-      </div>
-    </div>
-  );
+export default function Tabs({currentPage}: IProps) {
+    const router = useRouter();
+    const theme = useTheme();
+    return (
+        <div className={styles.root}>
+            <img
+                src={
+                    "https://lh3.googleusercontent.com/proxy/mdrER7gpHRg7DAj8ySS-yvlnJslI-EF5Vhoz6WgxJfoMqvTO_pJf_BAvcxp5TgSvIUH6SOXp_Tze4zllwC0Oui9NLEndIAn5Ty67cndDfs1G-dme7zpeNcTITmqIsw"
+                }
+                alt={"Logo"}
+            />
+
+            <div className={styles.buttons}>
+                <div style={{display:"flex", flexDirection:"row"}}>
+                    <DashboardIcon
+                        onClick={() => {
+                            router.push("/dashboard");
+                        }}
+                        style={{
+                            ...defaultStyles,
+                            color:
+                                currentPage === "dashboard"
+                                    ? theme.palette.primary.main
+                                    : "#5B5E86"
+                        }}
+                    />
+                    <div style={{
+                        ...currentTab,
+                        backgroundColor: theme.palette.primary.main,
+                        display: currentPage === "dashboard"?"inline":"none"
+                    }}/>
+            </div>
+                <div style={{display:"flex", flexDirection:"row"}}>
+                <SearchIcon
+                    onClick={() => {
+                        router.push("/search");
+                    }}
+                    style={{
+                        ...defaultStyles,
+                        color:
+                            currentPage === "search" ? theme.palette.primary.main : "#5B5E86"
+                    }}
+                />
+                    <div style={{
+                        ...currentTab,
+                        backgroundColor: theme.palette.primary.main,
+                        display: currentPage === "search"?"inline":"none"
+                    }}/>
+                </div>
+                <div style={{display:"flex", flexDirection:"row"}}>
+                <PortfolioIcon
+                    onClick={() => {
+                        router.push("/portfolio");
+                    }}
+                    style={{
+                        ...defaultStyles,
+                        color:
+                            currentPage === "portfolio"
+                                ? theme.palette.primary.main
+                                : "#5B5E86"
+                    }}
+                />
+                    <div style={{
+                        ...currentTab,
+                        backgroundColor: theme.palette.primary.main,
+                        display: currentPage === "portfolio"?"inline":"none"
+                    }}/>
+                </div>
+            </div>
+        </div>
+    );
 }
